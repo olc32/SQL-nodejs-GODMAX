af1a0079a125612bb0a3ab5521d1859c1504f0c0
olc32 <tegif89175@bmoar.com>
2026-04-20 17:45:44 +0200

    actu3_inestable

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 var juego = document.getElementById("juego");
 var body = document.body;
 var cube = document.getElementById("Cube");
-cube.addEventListener("click", function() {
-    cube.style.bottom = "100px";
+var gravity = 1;
+var velocity = 0;
+window.addEventListener("click", function() {
+    velocity = -15;
 });
-//Gravedad
-while (true) {
-    if (Intcube.style.bottom <= "0px") {
+
+function gameLoop() {
+    velocity += gravity;
+    cube.style.bottom = parseInt(cube.style.bottom) - velocity + "px";
+    if (parseInt(cube.style.bottom) <= 0) {
         cube.style.bottom = "0px";
+        velocity = 0;
     }
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
